5764d7973c21051512651073ca85df29fde05994
Emir <107634632+emirkvrak@users.noreply.github.com>
2023-02-15 21:59:51 +0300

    Add files via upload

---
+++ ArtikYilHesaplama/ArtikYilHesaplama.java
@@ -0,0 +1,35 @@
+package artikyilhesaplama;
+
+import java.util.Scanner;
+
+
+public class ArtikYilHesaplama {
+
+    
+    public static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+        
+        int yil;
+
+        System.out.print("Bir yil gir : ");
+        yil  = input.nextInt();
+        
+        if(yil % 4 == 0) {
+            if (yil % 100 == 0 ) {
+                if (yil % 400 == 0 ) {
+                    System.out.println(yil+" bir artık yıldır !");
+                }else
+                    System.out.println(yil+" bir artık yıl değildir !");
+            }
+            else {
+                System.out.println(yil+" bir artık yıldır !");
+            }
+        }
+        else {
+            System.out.println(yil+" bir artık yıl değildir !");
+        }
+        
+    }
+    
+}
